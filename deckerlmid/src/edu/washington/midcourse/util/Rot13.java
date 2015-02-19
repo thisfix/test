15cca4311e8a1d5ba58f6bbae39a9ba7bd415f13
thisfix <lukedecker1@gmail.com>
2015-02-19 12:31:42 -0800

    initial commit of skeleton project

---
+++ deckerlmid/src/edu/washington/midcourse/util/Rot13.java
@@ -0,0 +1,31 @@
+package edu.washington.midcourse.util;
+
+public class Rot13 {
+	public static String encrypt(String str){
+
+		char[] values = str.toCharArray();
+		for (int i = 0; i < values.length; i++) {
+		    char letter = values[i];
+
+		    if (letter >= 'a' && letter <= 'z') {
+		    	// Rotate lowercase letters.
+				if (letter > 'm') {
+				    letter -= 13;
+				} else {
+				    letter += 13;
+				}
+		    } else if (letter >= 'A' && letter <= 'Z') {
+		    	// Rotate uppercase letters.
+				if (letter > 'M') {
+				    letter -= 13;
+				} else {
+				    letter += 13;
+				}
+		    }
+		    values[i] = letter;
+		}
+		// Convert array to a new String.
+		return new String(values);
+	}
+
+}
---
+++ deckerlmid/src/edu/washington/midcourse/util/Rot13Test.java
@@ -0,0 +1,19 @@
+package edu.washington.midcourse.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Rot13Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
