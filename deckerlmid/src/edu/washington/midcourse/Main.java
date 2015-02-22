350d6cf4523143f2da09b54c632a668a49c0ecb3
thisfix <lukedecker1@gmail.com>
2015-02-21 19:58:15 -0800

    Main to main

--- deckerlmid/src/edu/washington/midcourse/Main.java
+++ deckerlmid/src/edu/washington/midcourse/Main.java
@@ -3,7 +3,7 @@ package edu.washington.midcourse;
 import edu.washington.midcourse.util.Rot13;
 
 public class Main {
-	public static void Main(String[] args){
+	public static void main(String[] args){
 		String encryptedString = "";
 		encryptedString = Rot13.encrypt(args[0]);
 		System.out.println("Standard String = "+args[0]);
